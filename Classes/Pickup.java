0f8220dfaacf9d85a10ababc9d802ef726f12696
max232st <max232st@gmail.com>
2023-08-03 10:50:31 +0300

    dz3

---
+++ Classes/Pickup.java
@@ -0,0 +1,38 @@
+package Classes;
+
+import java.awt.*;
+
+// Общий класс для пикапов
+public class Pickup extends Car implements iRefueling {
+    @Override
+    public void fuel() {
+
+    }
+
+    private int loadCapacity;
+    public Pickup(String make,
+                  String model,
+                  Color color,
+                  TypeCar bodyType,
+                  int numberWheels,
+                  TypeFuel fuel,
+                  TypeGearbox gearbox,
+                  float engineCap,
+                  int loadCapacity) {
+        super(make, model, color, bodyType, numberWheels, fuel, gearbox, engineCap);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public int gearShift(int gear) {
+        return 0;
+    }
+}
